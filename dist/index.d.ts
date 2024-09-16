53ec6ec6eefceab5e509685bb5a00e4f0d887ac1
Poliklot <poliklot@vk.com>
2024-09-16 10:14:01 +0300

    Добавлен .gitignore и удалены ненужные директории.

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare class ScrollFather {
-    private scrollListeners;
-    constructor(config?: Record<string, any>);
-    private init;
-    private handleScroll;
-    addScrollListener(element: HTMLElement, condition: (scrollPosition: number) => boolean, className: string): void;
-}
-export default ScrollFather;
